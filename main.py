9a890559a622408033472a77ce6ea74dd6f12583
mldarquea <mldarquea@uc.cl>
2022-03-28 22:44:18 -0300

    f1,f3

--- main.py
+++ main.py
@@ -21,7 +21,25 @@ df = df1[['date', "id", "user", "retweetCount"]].copy()
 
 # Función 1, top 10 tweets con más retweets
 def retweeted():
-    return
+    df_retweeted = df.sort_values(by=['retweetCount'], ascending=False).head(n=10)[['id','retweetCount']].copy()
+    return df_retweeted
 
-print(df.tail)
-df.info()
+
+# Función 3, días con más tweets
+def days():
+    return df.groupby(['date']).size()
+
+print("DATAFRAME ORIGINAL:\n")
+print(df.head(10))
+print("\n")
+# Función 1
+print("DATAFRAME ORDENADO SEGÚN PRIMERA FUNCIÓN:\n")
+print(retweeted())
+print("\n")
+
+
+# Función 3
+print("DATAFRAME ORDENADO SEGÚN TERCERA FUNCIÓN:\n")
+print(days().head(n=10))
+print("\n")
+# df.info()
